65652935b585b8f07bab24c9a07d9064fb975e3d
JH_Kim <86303543+yyoong2@users.noreply.github.com>
2023-02-08 12:26:39 +0900

    230208 ProjectListDAO 파일 생성

---
+++ Harvest/src/main/java/com/itwillbs/dao/ProjectListDAO.java
@@ -0,0 +1,11 @@
+package com.itwillbs.dao;
+
+import java.util.List;
+
+import com.itwillbs.domain.ProjectDTO;
+
+public interface ProjectListDAO {
+	
+	public List<ProjectDTO> getAllProjectList();
+
+}
---
+++ Harvest/src/main/java/com/itwillbs/dao/ProjectListDAOImpl.java
@@ -0,0 +1,25 @@
+package com.itwillbs.dao;
+
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.itwillbs.domain.ProjectDTO;
+
+@Repository
+public class ProjectListDAOImpl implements ProjectListDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace="com.itwillbs.mappers.projectListMapper";
+
+	@Override
+	public List<ProjectDTO> getAllProjectList() {
+		return sqlSession.selectList(namespace+".getAllProjectList");
+	}
+	
+	
+
+}
